7f6c55cd88779b585b0c90a0e77d7799cea11629
Evgeniy Rakashikov <evgeniyhd@gmail.com>
2023-01-31 20:50:08 +0600

    Created permissions

--- django/blog_api/views.py
+++ django/blog_api/views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from .permissions import PostUserWritePermission
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     queryset = Post.post_objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
