4167c2caba1fc5b98397bb2213635786434de3e1
Evgeniy Rakashikov <evgeniyhd@gmail.com>
2023-01-31 20:50:08 +0600

    Create permissions

--- django/blog_api/tests.py
+++ django/blog_api/tests.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from blog.models import Post, Category
 from django.contrib.auth.models import User
 
@@ -12,11 +12,27 @@ class PostTest(APITestCase):
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def createPost(self):
+    def testCreatePost(self):
         self.test_category = Category.objects.create(name='django')
-        self.test_user = User.objects.create_user(username='test_user1', password='123456789')
+        self.test_user = User.objects.create_superuser(username='test_user', password='123456789',)
+
+        self.client.login(username=self.test_user.username, password='123456789')
 
         data = {"title": "new", "author": 1, "excerpt": "new", "content": "new"}
         url = reverse('blog_api:list_create')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def testPostUpdate(self):
+        client = APIClient()
+
+        self.test_category = Category.objects.create(name='django')
+        self.test_user1 = User.objects.create_user(username='test_user1', password='123456789',)
+        self.test_user2 = User.objects.create_user(username='test_user2', password='123456789',)
+        self.test_post = Post.objects.create(category_id=1, title='Post Title', excerpt='Post Excerpt', content='Post Content', slug='post-title', author_id=1, status='published')
+
+        client.login(username=self.test_user2.username, password='123456789')
+
+        url = reverse('blog_api:detail_create', kwargs={'pk': 1})
+        response = client.put(url, {'title': 'Updated Title', 'content': 'Updated Content'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
