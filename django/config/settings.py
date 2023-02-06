da889a0345abbcf664e9fc387c81cacb95681ef1
Evgeniy Rakashikov <evgeniyhd@gmail.com>
2023-02-06 20:26:24 +0600

    Added API doc & updated pip req

--- django/config/settings.py
+++ django/config/settings.py
@@ -145,6 +145,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 CORS_ALLOWED_ORIGINS = [
